8a9bbc6773b52fa290bb3b71e9b5d8a85952ddda
96kennys <kent_nystedt@hotmail.com>
2014-11-12 18:14:00 +0100

    Push containing nothing
    123

--- RolePlayingGame/Modelclass.java
+++ RolePlayingGame/Modelclass.java
@@ -1,10 +1,13 @@
 package RolePlayingGame;
 
-public class  Modelclass{
+public class  Modelclass extends MainGUI{
 
 //Public metod skickar härifrån till GUI:et
 //"Allting händer här"
     public Modelclass(){
+        
+        do(
+                )while(MainGUI);
         
     }
 }
